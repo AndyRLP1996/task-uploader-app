f39db97b7b07d5f3a1e2f448ba0b3a3c21333fb2
Andrew Parkhurst <31877629+AndyRLP1996@users.noreply.github.com>
2019-03-30 21:40:37 -0700

    added file upload capability

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/create'
+  get 'tasks/destroy'
+  resources :tasks, only: [:index, :new, :create, :destroy]
+   root "tasks#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
